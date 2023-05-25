1faa52e46d7f52204d612a28b3566652f46b3936
Raiduy <radu.apsan@gmail.com>
2023-05-25 23:28:36 +0200

    a bunch of stuff, but it worc

--- packages/compute/run.py
+++ packages/compute/run.py
@@ -1,7 +1,14 @@
+#!/usr/bin/python3
+
 import json
 import os
 import pandas as pd
 import sys
+
+def give_row(dataset_path: str, row: int):
+  df = pd.read_csv(f"{dataset_path}/dataset.csv")
+  print(f'output: "{df.head(row).tail(1)}"')
+  return
 
 
 def main():
@@ -9,12 +16,14 @@ def main():
 
   if command == "first_row":
     df = pd.read_csv(f"{json.loads(os.environ['FILEPATH'])}/dataset.csv")
-    print(f'output:"{df.head(1)}"')
+    print(f'output:"first: {df.head(1)}"')
     return
 
   if command == "second_row":
     df = pd.read_csv(f"{json.loads(os.environ['FILEPATH'])}/dataset.csv")
-    return df.head(2)
+    print(f'output: "second: {df.head(2).tail(1)}"')
+    return
+  
 
 if __name__ == '__main__':
   main()

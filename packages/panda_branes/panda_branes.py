99ac7cb244562a904e3a9e17cfe9a93bfa5817ed
Raiduy <radu.apsan@gmail.com>
2023-06-04 23:51:01 +0200

    add panda_branes functions

--- packages/panda_branes/panda_branes.py
+++ packages/panda_branes/panda_branes.py
@@ -6,12 +6,202 @@ import json
 import os
 
 
-def hello_world(input):
-    print(f'output: "Hello, {input}!"')
-    
+'''
+Sample function
+'''
+def hello_world():
+    print('output: "Hello, World!"')
 
-def get_columns(data_path):
+
+'''--------------------DATA EXPLORATION FUNCTIONS--------------------'''
+
+# Single parameter functions.
+def columns(data_path: str):
     df = pd.read_csv(f'{data_path}/dataset.csv')
     columns = df.columns.tolist()
     print(f'output: "{str(columns)}"')
     return columns
+
+def index(data_path: str):
+    df = pd.read_csv(f'{data_path}/dataset.csv')
+    index = df.index.tolist()
+    print(f'output: "{str(index)}"')
+    return index
+
+def shape(data_path: str):
+    df = pd.read_csv(f'{data_path}/dataset.csv')
+    shape = df.shape
+    print(f'output: "{str(shape)}"')
+    return shape
+
+def dtypes(data_path: str):
+    df = pd.read_csv(f'{data_path}/dataset.csv')
+    dtypes = df.dtypes
+    print(f'output: "{str(dtypes)}"')
+    return dtypes
+
+def describe(data_path: str):
+    df = pd.read_csv(f'{data_path}/dataset.csv')
+    print(f'output: "{str(df.describe())}"')
+    return df.describe()
+
+
+# Two parameter functions.
+def head(data_path: str, n: int):
+    df = pd.read_csv(f'{data_path}/dataset.csv')
+    print(f'output: "{str(df.head(n))}"')
+    return df.head(n)
+
+def tail(data_path: str, n: int):
+    df = pd.read_csv(f'{data_path}/dataset.csv')
+    print(f'output: "{str(df.tail(n))}"')
+    return df.tail(n)
+
+def get_row(data_path: str, row: int):
+    df = pd.read_csv(f'{data_path}/dataset.csv')
+    index = df.loc[row].tolist()
+    print(f'output: "{str(index)}"')
+    return index
+
+def get_column(data_path: str, column_name: str):
+    df = pd.read_csv(f'{data_path}/dataset.csv')
+    column = df[column_name].tolist()
+    print(f'output: "{str(column)}"')
+    return column
+
+def loc(data_path: str, row: int, column: str):
+    df = pd.read_csv(f'{data_path}/dataset.csv')
+    print(f'output: "{str(df.loc[row, column])}"')
+    return df
+
+def iloc(data_path: str, row: int, column: int):
+    df = pd.read_csv(f'{data_path}/dataset.csv')
+    print(f'output: "{str(df.iloc[row, column])}"')
+    return df
+
+def describe_column(data_path: str, column_name: str):
+    df = pd.read_csv(f'{data_path}/dataset.csv')
+    column = df[column_name]
+    print(f'output: "{str(column.describe())}"')
+    return column.describe()
+
+def get_column_value_counts(data_path: str, column_name: str):
+    df = pd.read_csv(f'{data_path}/dataset.csv')
+    column = df[column_name]
+    print(f'output: "{str(column.value_counts())}"')
+    return column.value_counts()
+
+def query(data_path: str, query: str):
+    df = pd.read_csv(f'{data_path}/dataset.csv')
+    query_result = df.query(query)
+    print(f'output: "{str(query_result)}"')
+    return query_result
+
+'''-----------------------------------------------------------------------------------------------------------------'''
+
+
+
+
+'''--------------------DATA MANIPULATION FUNCTIONS--------------------'''
+
+# Sorting functions.
+def sort_index(data_path: str, axis: int, ascending: bool):
+    df = pd.read_csv(f'{data_path}/dataset.csv')
+    df = df.sort_index(axis=axis, ascending=ascending)
+    new_path = '/result/dataset.csv'
+    df.to_csv(new_path)
+    return new_path
+
+def sort_values(data_path: str, by: str, axis: int, ascending: bool):
+    df = pd.read_csv(f'{data_path}/dataset.csv')
+    df = df.sort_values(by=by, axis=axis, ascending=ascending)
+    new_path = '/result/dataset.csv'
+    df.to_csv(new_path)
+    return new_path
+
+
+# Cleaning functions.
+def drop_duplicates(data_path: str, subset: list):
+    df = pd.read_csv(f'{data_path}/dataset.csv')
+    df = df.drop_duplicates(subset=subset)
+    new_path = '/result/dataset.csv'
+    df.to_csv(new_path)
+    return new_path
+
+def dropna(data_path: str, axis: int):
+    df = pd.read_csv(f'{data_path}/dataset.csv')
+    df = df.dropna(axis=axis)
+    new_path = '/result/dataset.csv'
+    df.to_csv(new_path)
+    return new_path
+
+def fillna(data_path: str, value: str):
+    df = pd.read_csv(f'{data_path}/dataset.csv')
+    df = df.fillna(value=value)
+    new_path = '/result/dataset.csv'
+    df.to_csv(new_path)
+    return new_path
+
+def drop(data_path: str, labels: list, axis: int):
+    df = pd.read_csv(f'{data_path}/dataset.csv')
+    df = df.drop(labels=labels, axis=axis)
+    new_path = '/result/dataset.csv'
+    df.to_csv(new_path)
+    return new_path
+
+def drop_column(data_path: str, column_name: str):
+    df = pd.read_csv(f'{data_path}/dataset.csv')
+    df = df.drop(columns=column_name)
+    new_path = '/result/dataset.csv'
+    df.to_csv(new_path)
+    return new_path
+
+
+# Column manipulation functions.
+def cast_column(data_path: str, column_name: str, dtype: str):
+    df = pd.read_csv(f'{data_path}/dataset.csv')
+    df[column_name] = df[column_name].astype(dtype)
+    new_path = '/result/dataset.csv'
+    df.to_csv(new_path)
+    return new_path
+
+def rename_column(data_path: str, old_column_name: str, new_column_name: str):
+    df = pd.read_csv(f'{data_path}/dataset.csv')
+    df = df.rename(columns={old_column_name: new_column_name})
+    new_path = '/result/dataset.csv'
+    df.to_csv(new_path)
+    return new_path
+
+# https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.query.html
+# (I actually didn't know this function existed)
+def query_and_save(data_path: str, query: str):
+    df = pd.read_csv(f'{data_path}/dataset.csv')
+    df = df.query(query)
+    new_path = '/result/dataset.csv'
+    df.to_csv(new_path)
+    return new_path
+
+
+'''-----------------------------------------------------------------------------------------------------------------'''
+
+
+
+
+'''--------------------MULTI-DATASET FUNCTIONS--------------------'''
+def concat(data_path: str, other_data_path: str, axis: int):
+    df1 = pd.read_csv(f'{data_path}/dataset.csv')
+    df2 = pd.read_csv(f'{other_data_path}/dataset.csv')
+    df = pd.concat([df1, df2], axis=axis)
+    new_path = '/result/dataset.csv'
+    df.to_csv(new_path)
+    return new_path
+
+def merge(data_path: str, other_data_path: str, on: str, how: str):
+    df1 = pd.read_csv(f'{data_path}/dataset.csv')
+    df2 = pd.read_csv(f'{other_data_path}/dataset.csv')
+    df = pd.merge(df1, df2, on=on, how=how)
+    new_path = '/result/dataset.csv'
+    df.to_csv(new_path)
+    return new_path
+
+'''-----------------------------------------------------------------------------------------------------------------'''
--- packages/panda_branes/run.py
+++ packages/panda_branes/run.py
@@ -6,22 +6,26 @@ import json
 import os
 import sys
 
-
-def run_action(cmd, filepath):
+def run_action_two_parameters(cmd, filepath, parameter1):
   return {
-    "get_columns": panda_branes.get_columns,
-  }[cmd](filepath)
+    "get_column": panda_branes.get_column,
+    "describe_column": panda_branes.describe_column,
+    "get_column_value_counts": panda_branes.get_column_value_counts,
+  }[cmd](filepath, parameter1)
 
 
 def main():
   command = sys.argv[1]
 
   if command == "hello_world":
-    # pandabranes.hello_world(json.loads(os.environ['FILEPATH']))
-    print(f'output: "Hello, World!"')
+    panda_branes.hello_world()
     return
 
-  f_out = run_action(command, json.loads(os.environ['FILEPATH']))
+  elif command == "get_column":
+    out = panda_branes.get_column(f"{json.loads(os.environ['FILEPATH_DATA'])}", f"{json.loads(os.environ['COLUMN_NAME'])}")
+    return
+
+  f_out = run_action_two_parameters(command, json.loads(os.environ['FILEPATH']), json.loads(os.environ['PARAMETER1']))
 
 
 if __name__ == '__main__':
